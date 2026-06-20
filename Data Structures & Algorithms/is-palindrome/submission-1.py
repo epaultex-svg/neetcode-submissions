eb691336cc72f36c13a5d98e4e508ad793ec7c83
epaultex-svg <epaultex@gmail.com>
2026-06-19 22:15:03 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right :
+            while not s[right].isalnum() and (right > left):
+                right -= 1
+            while not s[left].isalnum() and (left < right):
+                left += 1
+            if not s[left].lower() == s[right].lower():
+                return False
+            right -= 1
+            left += 1
+        return True
+
+
+        "R, a ce, ca r ,"
