bafd6ffa09a9ee00078ca008b142fc6156403f01
epaultex-svg <epaultex@gmail.com>
2026-07-30 22:37:01 -0500

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        q = deque([root])
+        res = []
+
+        while q:
+            lvlSize = len(q)
+
+            for i in range(lvlSize):
+                node = q.popleft()
+                print((i,lvlSize))
+
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if i == lvlSize - 1:
+                    res.append(node.val)
+            
+        return res        
+
