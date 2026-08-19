312f904ad74b9d2fc988efaa5643f3e4ad025f3f
epaultex-svg <epaultex@gmail.com>
2026-08-18 22:51:29 -0500

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        preMap = {0:1}
+        curSum = 0
+
+        for num in nums:
+
+            curSum += num
+            diff = curSum - k
+
+            res += preMap.get(diff,0)
+            preMap[curSum] = 1 + preMap.get(curSum,0)
+
+        return res
