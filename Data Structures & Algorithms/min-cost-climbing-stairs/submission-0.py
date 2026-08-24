fff65a0716663300d16e0a2be1e0a5b3fd14369c
epaultex-svg <epaultex@gmail.com>
2026-08-23 19:43:54 -0500

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        cache = [-1] * len(cost)
+        top = len(cost)
+
+        def dfs(i):
+            if i >= top:
+                return 0
+            if cache[i] != -1:
+                return cache[i]
+            c = cost[i]
+            cache[i] = c + min(dfs(i+1), dfs(i+2))
+            return cache[i]
+
+        return min(dfs(0),dfs(1))
+            
+        
